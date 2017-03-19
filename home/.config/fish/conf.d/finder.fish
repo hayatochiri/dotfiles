a40abefc60b03b811b1ffaeeefd8aaf7d4d99e16
hayatochiri <dev@sucret.sakura.ne.jp>
2017-03-19 21:04:27 +0900

    fishのfinder関連設定を追加

---
+++ home/.config/fish/conf.d/finder.fish
@@ -0,0 +1,38 @@
+function cd
+  if [ -n "$argv" ]
+    cd $argv
+    return
+  end
+  while true
+    clear
+    ls -a
+    _dirs | fzf --height 5% --border --exit-0 | read -l result
+    switch "$result"
+    case '.'
+      return
+    case ''
+      return
+    case '*'
+      builtin cd $result
+    end
+  end
+end
+
+# unlimited cd
+function ucd
+  if [ -z "$argv" ]
+    set argv '.'
+  end
+  find $argv -type d 2>/dev/null | fzf --exit-0 | read -l result
+  if [ -z "$result" ]
+    return
+  end
+  builtin cd $result
+end
+
+function _dirs
+  echo '.'
+  echo '..'
+  find . -maxdepth 1 -type d 2>/dev/null | grep -v '^\.$'
+  find / -maxdepth 1 -type d 2>/dev/null
+end
