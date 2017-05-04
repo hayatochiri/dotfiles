444b795c265b337ff004cd34b0df04db86ad98c7
hayatochiri <dev@sucret.sakura.ne.jp>
2017-05-05 00:16:51 +0900

    fish: プロンプトでgit worktreeにいるかどうかを判定できるように変更

--- home/.config/fish/conf.d/prompt.fish
+++ home/.config/fish/conf.d/prompt.fish
@@ -1,6 +1,6 @@
-# function fish_prompt
-#   echo 'hoge>'
-# end
+function fish_prompt
+  set IS_REPOSITORY (command git rev-parse --is-inside-work-tree 2>/dev/null)
+end
 
 # function fish_right_prompt
 #   echo '<hoge'
