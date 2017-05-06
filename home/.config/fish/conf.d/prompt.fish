c2bb8fd719b319c2cf09dbbf34807be4fee8f636
hayatochiri <dev@sucret.sakura.ne.jp>
2017-05-06 13:21:27 +0900

    fish: プロンプトにリポジトリまでのパス、リポジトリからのパスを表示するように変更

--- home/.config/fish/conf.d/prompt.fish
+++ home/.config/fish/conf.d/prompt.fish
@@ -1,7 +1,23 @@
 function fish_prompt
   set IS_REPOSITORY (command git rev-parse --is-inside-work-tree 2>/dev/null)
+
+  if [ "$IS_REPOSITORY" = 'true' ]
+    _prompt_git
+  else
+    echo -n (pwd)
+  end
+
+  echo -e "\n>"
 end
 
 # function fish_right_prompt
 #   echo '<hoge'
 # end
+
+function _prompt_git
+  set TOPLEVEL (command git rev-parse --show-toplevel)
+  set PREFIX (command git rev-parse --show-prefix)
+  set BRAnCh (command git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+
+  echo -n "$TOPLEVEL > $BRAnCh > $PREFIX"
+end
