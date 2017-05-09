0e3aff4209f4d2108f1cc26a0c69e4a21d261400
hayatochiri <dev@sucret.sakura.ne.jp>
2017-05-09 18:45:30 +0900

    fish: git add補助コマンドで選択するファイルのパスにスペースが含まれていても対応できるように修正

--- home/.config/fish/conf.d/git.fish
+++ home/.config/fish/conf.d/git.fish
@@ -80,7 +80,7 @@ end
 function _git_add
   echo $argv
   set -e result
-  _git_status_colorize| fzf --exit-0 --ansi --multi --bind="$git_fzf_binds,ctrl-a:select-all" --preview='git diff --color {2}' | awk '{print $2;}' | while read -l r
+  _git_status_colorize| fzf --exit-0 --ansi --multi --bind="$git_fzf_binds,ctrl-a:select-all" --preview='git diff --color {2..-1}' | string sub -s 4 | while read -l r
     set result $result $r
   end
   echo "git $argv $result"
