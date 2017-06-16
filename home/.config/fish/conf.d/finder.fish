7e6222f5214ee2151f97a67881a1af503945475c
hayatochiri <dev@sucret.sakura.ne.jp>
2017-06-16 18:41:42 +0900

    fish: cdgコマンドのプレビューを画面上部に表示するように変更

--- home/.config/fish/conf.d/finder.fish
+++ home/.config/fish/conf.d/finder.fish
@@ -52,7 +52,7 @@ end
 function cdg
   set PREFIX (command git rev-parse --show-prefix)
   set TOPLEVEL (command git rev-parse --show-toplevel)
-  command git ls-files "$TOPLEVEL" --full-name | fzf --query="$PREFIX" --preview="dirname $TOPLEVEL/{}; echo; fish -c '_lsl_color (dirname '$TOPLEVEL/{}')'" | read -l result
+  command git ls-files "$TOPLEVEL" --full-name | fzf --query="$PREFIX" --preview-window=top:50% --preview="dirname $TOPLEVEL/{}; echo; fish -c '_lsl_color (dirname '$TOPLEVEL/{}')'" | read -l result
   test -z "$result"; and return
   cd "$TOPLEVEL/$result"
 end
