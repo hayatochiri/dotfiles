67d4f5143e4f2135bd228575fcf494607feeabee
hayatochiri <dev@sucret.sakura.ne.jp>
2017-08-21 13:29:24 +0900

    fish: Sublime text 2でgit difftoolを起動する際の待機時間を調整

--- home/.config/fish/conf.d/sublime-text.fish
+++ home/.config/fish/conf.d/sublime-text.fish
@@ -4,7 +4,7 @@ function subl2
     /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl --command "sublimerge_diff {\"left\":\"$argv[1]\",\"right\":\"$git_root/$argv[2]\"}"
 
     # Sublime Text 2 で一時ファイルを開く前に git difftool が一時ファイルを削除してしまうのでその対策
-    sleep 0.1
+    sleep 1
   else
     vimdiff $argv[1] "$git_root/$argv[2]"
   end
